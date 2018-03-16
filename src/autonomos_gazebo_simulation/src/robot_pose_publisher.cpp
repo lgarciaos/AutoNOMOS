5a09454333890f3f7618432cf4399102dc85fc0c
Edgar Granados Osegueda <edgar.granados@itam.mx>
2018-03-15 21:26:32 -0600

    vision working with curved road

--- src/autonomos_gazebo_simulation/src/robot_pose_publisher.cpp
+++ src/autonomos_gazebo_simulation/src/robot_pose_publisher.cpp
@@ -7,9 +7,6 @@
 #include <iostream>
 #include <stdio.h>
 #include <Eigen/Geometry>
-#include "gazebo_msgs/ApplyJointEffort.h"
-#include "gazebo_msgs/JointRequest.h"
-#include "gazebo_msgs/LinkStates.h"
 #include "tf/transform_datatypes.h"
 #include "tf/LinearMath/Matrix3x3.h"
 #include <tf2_ros/transform_listener.h>
@@ -83,50 +80,7 @@ void get_speed(const std_msgs::Int16& msg) {
 	 speed = msg.data;
 }
 
-void get_curr_link_states(const gazebo_msgs::LinkStates& msg)
-{
-	int speed_left_link = -1;
-	int speed_right_link = -1;
-	int steering_link = -1;
-	double roll, pitch, yaw;
 
-	std::string steering_link_name = car_name + "::steer_link";
-	std::string speed_left_link_name =  car_name + "::back_left_wheel_link";
-	std::string speed_right_link_name = car_name + "::back_right_wheel_link";
-	// ROS_INFO_STREAM("getting link states:" << msg);
-	// ROS_INFO_STREAM("size: " << msg.name.size());
-	for (int i = 0; i < msg.name.size(); ++i)
-	{
-		if(!steering_link_name.compare(msg.name[i]) )
-		{
-			ROS_DEBUG_STREAM("steering link is: " << i);
-			steering_link = i;
-		}
-		if(!speed_left_link_name.compare(msg.name[i]) )
-		{
-			ROS_DEBUG_STREAM("speed left link is: " << i);
-		}
-		if(!speed_right_link_name.compare(msg.name[i]) )
-		{
-			ROS_DEBUG_STREAM("speed right link is: " << i);
-		}
-	}
-	geometry_msgs::Quaternion qm = msg.pose[steering_link].orientation;
-	tf::Quaternion q(qm.x, qm.y, qm.z, qm.w);
-	// ROS_INFO_STREAM(msg.pose[steering_link].orientation) ;
-	// ROS_INFO_STREAM(q);
-	tf::Matrix3x3 m(q);
-
-	pose_stp_w.header.frame_id = "ackermann";
-	pose_stp_w.pose.position = msg.pose[steering_link].position;
-	pose_stp_w.pose.orientation = msg.pose[steering_link].orientation;
-	
-	m.getRPY(roll, pitch, yaw); //USING yaw
-	ROS_INFO_STREAM("( " << roll << " , " << pitch << " , " << -yaw << " )");
-	ROS_INFO_STREAM("rads: " << -yaw << " deg: " << -yaw * 180/ M_PI);
-	curr_steering = -yaw * 180 / M_PI;
-
-}
 int main(int argc, char **argv){
 	ros::init(argc,argv,"robot_velocity_node");
 	ros::NodeHandle nh("~");
@@ -146,24 +100,12 @@ int main(int argc, char **argv){
 
 	nh.param<float>("st_ki", ki, 0.0001);
 	ROS_INFO_STREAM("The steering ki values is: " << ki);
-	// if(argc > 1)
-	// {
-	// 	car_name = argv[2];
-	// }
+
 	nh.param<std::string>("car_name", car_name, "ackermann");
 	ROS_INFO_STREAM("Using gazebo model: " << car_name);
 	// Suscribe to Gazebo service ApplyJointEffor
-	ros::ServiceClient client = nh.serviceClient<gazebo_msgs::ApplyJointEffort>("/gazebo/apply_joint_effort");
-	ros::ServiceClient client_clr = nh.serviceClient<gazebo_msgs::JointRequest>("/gazebo/clear_joint_forces");
-	gazebo_msgs::ApplyJointEffort eff_msg[4];
+
 	
-	gazebo_msgs::JointRequest jr;
-	jr.request.joint_name = "steer_joint";
-
-	ros::Subscriber sub_steering = nh.subscribe("/manual_control/steering", 1, &get_steering);
-	ros::Subscriber sub_speed = nh.subscribe("/manual_control/speed", 1, &get_speed);
-	// ros::Subscriber sub_curr_link_states = nh.subscribe("/gazebo/link_states", 1, &get_curr_link_states);
-
 	ros::Publisher pub_cur = nh.advertise<geometry_msgs::Pose2D> ("/robot/pose", 1);
 	ros::Publisher pub_des = nh.advertise<std_msgs::Float64> ("/car_des_steering_angle", 1);
 	std_msgs::Float64 msg_fl;
