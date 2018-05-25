36e1d192b674717cd8e19a640239399daf42636d
Eduardo Mtz <eduardodsat@hotmail.com>
2018-05-25 15:23:55 -0500

    cambios menores en launch file por actualizacion de modelo

--- frenar.sh
+++ frenar.sh
@@ -3,4 +3,4 @@
 rostopic pub -1 /steering std_msgs/Int16 "data: 90"
 rostopic pub -1 /manual_control/speed std_msgs/Int16 "data: 0"
 rostopic pub -1 /AutoNOMOS_mini/manual_control/steering std_msgs/Int16 "data: 45"
-rostopic pub -1 /AutoNOMOS_mini/manual_control/velocity std_msgs/Int16 "data: 0"
+rostopic pub -1 /AutoNOMOS_mini/manual_control/speed std_msgs/Int16 "data: 0"
