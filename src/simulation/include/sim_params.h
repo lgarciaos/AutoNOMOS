6714fe40b3cb4009ab5a4c40d90e677f31bbc9d5
Edgar Granados <gary.granados@gmail.com>
2018-08-07 19:29:54 -0400

    created namespace to store the simulation related variables

---
+++ src/simulation/include/sim_params.h
@@ -0,0 +1,22 @@
+#ifndef SIMULATION_PARAMS
+#define SIMULATION_PARAMS
+
+#include <string>
+
+// simulation,  true);
+// sim_iters,  1);
+// s", gz_total_lines,  0);
+// plot_lines,  false);
+// obstacles_radius,  0);
+namespace sim_params
+{
+	extern bool simulation;
+	// extern int sim_iters;
+	extern int gz_total_lines;
+  extern bool plot_lines;
+	extern bool publish_ctrl_path;
+}
+
+
+
+#endif
