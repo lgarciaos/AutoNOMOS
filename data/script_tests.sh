e833322c9430140e3eaea805446fa1843befc380
Edgar Granados <gary.granados@gmail.com>
2018-08-10 15:27:42 -0400

    added param to prevent plotting lines

--- data/script_tests.sh
+++ data/script_tests.sh
@@ -12,22 +12,29 @@ planner="SST"
 
 
 cont_planner=1
-until [ $cont_planner -gt 1 ] # 2
+until [ $cont_planner -gt 2 ] # 2
 do
-  iters=10000 #1000000
+  iters=1000000
   counter_ext=1
   rosservice call /gazebo/reset_simulation "{}"
-  until [ $counter_ext -gt 1 ] # 12
+  until [ $counter_ext -gt 12 ] # 12
   do
     counter_int=1
     rosservice call /gazebo/reset_simulation "{}"
-    until [ $counter_int -gt 1 ] # 10
+    until [ $counter_int -gt 10 ] # 10
     do
       rosservice call /gazebo/reset_simulation "{}"
       echo $planner" iters: "$iters" count: "$counter_int" using: "$1" to: ("$2", "$3", "$4")."
-      roslaunch simulation dummy.launch stopping_check_in:=$iters \
-        planner_in:=$planner publish_ctrl_path_in:=false ctrl_to_use_in:=$1 \
-        goal_state/x:=$2 goal_state/y:=$3 goal_state/theta:=$4 &>> $home_$file_aux
+      roslaunch simulation dummy.launch
+        stopping_check_in:=$iters \
+        planner_in:=$planner \
+        publish_ctrl_path_in:=false \
+        ctrl_to_use_in:=$1 \
+        goal_state/x:=$2 \
+        goal_state/y:=$3 \
+        goal_state/theta:=$4 \
+        gz_plot_lines_in:=false \
+        &>> $home_$file_aux
 
         # add code to check that there is new data after the roslaunch command
         # it could be done using the output of the cmd awk '/Solution Quality'
